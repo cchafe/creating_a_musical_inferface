4daa8eff1d319a79c0cb4b9985123a83b65c4caa
Chris Chafe <cchafe@github.com>
2022-06-15 09:47:33 -0700

    connects by addr or name, rcvs emphemeral UDP port

--- networkSockets/tcp.cpp
+++ networkSockets/tcp.cpp
@@ -1,5 +1,7 @@
 #include "tcp.h"
 #include <iostream>
+#include <QtEndian>
+#include <QHostInfo>
 
 TCP::TCP()
 {
@@ -8,20 +10,31 @@ TCP::TCP()
 
 void TCP::connectToHost()
 {
-    socket->connectToHost(QString("127.0.0.1"),5000);
+    QHostAddress serverHostAddress;
+//    QString server("cmn9.stanford.edu");
+    QString server("171.64.197.158");
+    if (!serverHostAddress.setAddress(server)) {
+        QHostInfo info = QHostInfo::fromName(server);
+        if (!info.addresses().isEmpty()) {
+            // use the first IP address
+            serverHostAddress = info.addresses().constFirst();
+        }
+    }
+    socket->connectToHost(serverHostAddress,4464);
     socket->waitForConnected(500);
 }
 
 void TCP::sendToHost()
 {
     if (socket->state()==QTcpSocket::ConnectedState) {
-        QString request("hi");
+        QString request("4464");
         QByteArray ba;
         ba.append(request);
         ba.append('\n');
         socket->write(ba);
         socket->waitForBytesWritten(1000);
         //    fprintf(stderr,"wrote :%s\n",request.toLocal8Bit().data());
+        readyRead();
     } else
         std::cout << "TCP: tried to send data but not connected to server" << std::endl;
 
@@ -29,7 +42,32 @@ void TCP::sendToHost()
 
 void TCP::readyRead()
 {
+    socket->waitForReadyRead();
     quint16 bytes =  socket->bytesAvailable();
+    std::cout << "TCP: from server " << bytes << std::endl;
+
+    uint32_t udp_port;
+    int size       = sizeof(udp_port);
+    char* port_buf = new char[size];
+    socket->read(port_buf, size);
+    udp_port = qFromLittleEndian<qint32>(port_buf);
+
+    std::cout << "TCP: ephemeral port = " << udp_port << std::endl;
+
+    //    ui->outputTextEdit->append("----------------------------------------");
+    //    ui->outputTextEdit->append(QString::number(bytes)+ " for you to read");
+    //    ui->outputTextEdit->append("----------------------------------------");
+    //    QByteArray ba= socket->readAll();
+    //    QString dataString (ba);
+    //    QString pos("<source id='5'><position");
+    //    if (dataString.contains(pos)) {
+    //        ui->outputTextEdit->append(dataString);
+    //        QString request0("<request><source id='2'><position x='0.0' y='0.0'/></source></request>" );
+    //        QByteArray ba;
+    //        ba.append(request0);
+    //        ba.append('\0');
+    //        socket->write(ba);
+    //    }
 }
 
 
